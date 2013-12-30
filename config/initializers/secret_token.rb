0f0ee238cb4a4522779b4c9fa1856b6829ea5836
kishore <kishore_personal1@yahoo.com>
2013-12-30 23:25:58 +0530

    created welcome page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KalyanBlog::Application.config.secret_token = '055f3295706b425e14f34baad5df2b4a1bd82dee50595cc9aaa55a3819516e3fac451c4f152e5235993a6a011c052ca9a011fe0da3642b2df00c209bc59f966a'
